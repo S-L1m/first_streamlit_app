52a7c83274b494d49cd329d2d1b076c949af83e3
S-L1m <84104956+S-L1m@users.noreply.github.com>
2023-07-01 21:12:53 +1200

    Update streamlit_app.py
    added a streamlit.stop for debugging

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,8 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # Output as a dataframe
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
