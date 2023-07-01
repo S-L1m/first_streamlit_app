43791292d592e6774053344961977e2a375b2ef9
S-L1m <84104956+S-L1m@users.noreply.github.com>
2023-07-01 21:27:46 +1200

    Update streamlit_app.py
    minor bug fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ def get_fruityvice_data(this_fruit_choice):
   return fruityvice_normalized
 
 def get_fruit_load_list():
-  with my_cur as my_cnx.cursor():
+  with my_cnx.cursor() as my_cur:
     my_cur.execute("SELECT * from fruit_load_list")
     return my_cur.fetchall()
 
